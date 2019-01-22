073efd83b907f4f5b6d0e5b008e1b456faa7f6e7
Crystal Rhee <crystal.rhee@gmail.com>
2019-01-22 15:07:57 -0500

    return all users

---
+++ src/main/java/com/example/cs4500sp19s2rhees/services/UserService.java
@@ -0,0 +1,23 @@
+package com.example.cs4500sp19s2rhees.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cs4500sp19s2rhees.models.User;
+
+@RestController
+public class UserService {
+	static List<User> users = new ArrayList<User>();
+	static {
+		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
+		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+	}
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return users;
+	}
+}
