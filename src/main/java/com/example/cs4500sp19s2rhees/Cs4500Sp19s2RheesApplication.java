5b8f6a2faea9b7d1a581aa83c259a54c017c8373
Crystal Rhee <crystal.rhee@gmail.com>
2019-01-22 15:09:31 -0500

    update main init to run user services

--- src/main/java/com/example/cs4500sp19s2rhees/Cs4500Sp19s2RheesApplication.java
+++ src/main/java/com/example/cs4500sp19s2rhees/Cs4500Sp19s2RheesApplication.java
@@ -1,4 +1,4 @@
-package edu.neu.cs4500;
+package com.example.cs4500sp19s2rhees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
